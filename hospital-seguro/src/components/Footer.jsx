cf2960df9b95de494a8b36be7017c3df1cf9b760
Andres2023STL <123616056+Andres2023STL@users.noreply.github.com>
2025-02-02 12:52:59 -0600

    Agregue los components

---
+++ hospital-seguro/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      © 2025 Sistema Integrado. Todos los derechos reservados.
+    </footer>
+  );
+}
+
+export default Footer;
