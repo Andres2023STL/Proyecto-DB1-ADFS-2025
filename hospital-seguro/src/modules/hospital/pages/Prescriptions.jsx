5cf73c2616a4b6045fe9421f11fb7e8b297e45b2
Andres2023STL <123616056+Andres2023STL@users.noreply.github.com>
2025-02-02 13:38:10 -0600

    Agregar pages a hospital

---
+++ hospital-seguro/src/modules/hospital/pages/Prescriptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Prescriptions() {
+  return (
+    <div className="page-container">
+      <h1>Gestión de Recetas</h1>
+      <p>Crea y administra las recetas médicas.</p>
+    </div>
+  );
+}
+
+export default Prescriptions;
