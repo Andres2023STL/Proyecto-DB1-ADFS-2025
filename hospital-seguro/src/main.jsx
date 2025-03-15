b1086dc52434ba4b57473fa55033699492e6a8f3
WOG15 <126122733+WOG15@users.noreply.github.com>
2025-03-15 11:47:52 -0600

    Ajustes frontend
    Añadido chakra y solucionado el problema de alineación de las imágenes

--- hospital-seguro/src/main.jsx
+++ hospital-seguro/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/global.css';  // Estilos globales
-
+import { Provider } from "./components/ui/provider";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Provider>
         <App />
+      </Provider>  
     </BrowserRouter>
   </React.StrictMode>
 );
