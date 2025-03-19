2b852e8726b687b7fc43676c753cabee722c24c1
Andres2023STL <123616056+Andres2023STL@users.noreply.github.com>
2025-03-19 12:09:03 -0600

    Modificación de carpeta components
    Se modificaron archivos de components ya que se tiene que ajustar a los cambios.

--- hospital-seguro/src/components/Header.jsx
+++ hospital-seguro/src/components/Header.jsx
@@ -1,26 +1,10 @@
-  import React from 'react';
-  import { Link } from 'react-router-dom';
-  import '../styles/Header.css';
+const Header = () => {
+  return (
+    <header>
+      <img src="/logo.png" alt="Logo"/>
+      <h1>Mi Plataforaama</h1>
+    </header>
+  );
+};
 
-  function Header() {
-    const role = localStorage.getItem('role');
-
-    return (
-      <header className="header">
-        <h1>Plataforma de Gestión Médica</h1>
-        <img src="/home-heart.png" alt="Logo" className='logo'/>
-        <nav>
-          <Link to="/" className="header-link">Inicio</Link>
-          <Link to="/hospital" className="header-link">Hospital</Link>
-          <Link to="/seguro" className="header-link">Seguro</Link>
-          <Link to="/historia" className="header-link">Historia</Link>
-          <Link to="/faq" className="header-link">FAQ</Link>
-          <Link to="/contacto" className="header-link">Contacto</Link>
-          {!role && <Link to="/login" className="header-link">Login</Link>}
-          {role && <Link to="/dashboard" className="header-link">Dashboard</Link>}
-        </nav>
-      </header>
-    );
-  }
-
-  export default Header;
+export default Header;
---
+++ hospital-seguro/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await fetch("http://localhost/hospital_api/logout.php", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    setIsAuthenticated(false);
+    navigate("/login");
+  };
+
+  return (
+    <nav>
+      <Link to="/">Inicio</Link>  
+      <Link to="/SubHomeHospital">Hospital</Link> 
+      <Link to="/SubHomeSeguro">Seguro</Link> 
+      <Link to="/AboutUs">Acerca de nosotros</Link> 
+
+      {isAuthenticated ? (
+        <>
+          <Link to="/hospital/dashboard">Dashboard</Link>
+          <button onClick={handleLogout}>Cerrar Sesión</button>
+        </>
+      ) : (
+        <Link to="/Login">Inicio de Sesión</Link>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
