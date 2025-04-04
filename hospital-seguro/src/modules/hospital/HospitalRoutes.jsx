99fbd5106cc802868d573d16d7c65497a438704b
Andres2023STL <123616056+Andres2023STL@users.noreply.github.com>
2025-04-03 18:28:31 -0600

    hospitalroutes
    se eliminaron y añadieron nuevas rutas

--- hospital-seguro/src/modules/hospital/HospitalRoutes.jsx
+++ hospital-seguro/src/modules/hospital/HospitalRoutes.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import DoctorProfileForm from "./pages/doctor/DoctorProfileForm";
+import PacienteProfileForm from "./pages/paciente/PacienteProfileForm";
 import DoctorDashboard from "./pages/doctor/DoctorDashboard";
-import Appointments from "./pages/doctor/Appointments";
-import DoctorCatalog from "./pages/doctor/DoctorCatalog";
-import DoctorDetails from "./pages/doctor/DoctorDetails";
+import Appointments from "./pages/doctor/DoctorAppointments";
+import DoctorAppointments from "./pages/doctor/DoctorAppointments";
 import PatientHistory from "./pages/doctor/PatientHistory";
 import Recetas from "./pages/doctor/Recetas";
 import HospitalEmpleadoDashboard from "./pages/empleado_hospital/HospitalEmpleadoDashboard";
 import PatientRegister from "./pages/empleado_hospital/PatientRegister";
+import PacienteDashboard from "./pages/paciente/PacienteDashboard";
 
 
 const HospitalRoutes = () => {
@@ -16,13 +17,14 @@ const HospitalRoutes = () => {
       {/* Define las rutas para la sección de hospital, cada <Route> mapea una dirección a un componente específico */}
       <Route path="dashboard" element={<DoctorDashboard />} />
       <Route path="appointments" element={<Appointments />} />
-      <Route path="doctorcatalog" element={<DoctorCatalog />} />
-      <Route path="doctordetails/:id" element={<DoctorDetails />} />
+      <Route path="DoctorAppointments" element={<DoctorAppointments />} />
       <Route path="patienthistory" element={<PatientHistory />} />
       <Route path="recetas" element={<Recetas />} />
       <Route path="HospitalEmpleadoDashboard" element={<HospitalEmpleadoDashboard />} />
       <Route path="patientregister" element={<PatientRegister />} />
       <Route path="DoctorProfileForm" element={<DoctorProfileForm />} />
+      <Route path="PacienteProfileForm" element={<PacienteProfileForm />} />
+      <Route path="PacienteDashboard" element={<PacienteDashboard />} />
       
       <Route path="*" element={<p>Ruta no encontrada en HospitalRoutes</p>} />
     </Routes>
