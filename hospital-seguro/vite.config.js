7beacc8421730ee534e6e37c3087d57a739c2935
Andres2023STL <123616056+Andres2023STL@users.noreply.github.com>
2025-04-07 22:52:26 -0600

    Eliminar
    Se eliminaron archivos y carpetas para una mejor estructura y practica en el código.

--- hospital-seguro/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Configuración básica para Vite con React
-export default defineConfig({
-  plugins: [react()],
-});
